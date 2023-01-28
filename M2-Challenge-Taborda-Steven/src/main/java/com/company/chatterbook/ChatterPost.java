cd38024e18e7d923e7ca82fff09d84201249b0c7
Steven T <32182614+staborda-nmsu@users.noreply.github.com>
2023-01-28 14:39:02 -0700

    Module 2 Challenge

---
+++ M2-Challenge-Taborda-Steven/src/main/java/com/company/chatterbook/ChatterPost.java
@@ -0,0 +1,18 @@
+package com.company.chatterbook;
+
+public class ChatterPost {
+    private String text;
+
+    public ChatterPost(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
+
---
+++ M2-Challenge-Taborda-Steven/src/main/java/com/company/chatterbook/User.java
@@ -0,0 +1,29 @@
+package com.company.chatterbook;
+
+import java.util.List;
+
+public class User {
+    private String name;
+    private List<ChatterPost> chatterPosts;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<ChatterPost> getChatterPosts() {
+        return chatterPosts;
+    }
+
+    public void setChatterPosts(List<ChatterPost> chatterPosts) {
+        this.chatterPosts = chatterPosts;
+    }
+}
+
